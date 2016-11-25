05e070c48c613156090b39f5da73b78cd1a01760
DanTNC <fad11204@yahoo.com.tw>
2016-11-26 01:29:38 +0800

    Update MSOEctr.js

--- MSOEctr.js
+++ MSOEctr.js
@@ -4,15 +4,15 @@ var Dstate=0.0; //n=-5~4. n=0 for N=1, n+1=>N*2, n-1=>N/2. n.1=N*(1+1/2), n.2=N*
 var key = () => {
 	switch(event.keyCode){
 		case 44:
-			if(Dstate>-5)
-				Dstate=Dstate-1;
+			if(Dstate>-5.0)
+				Dstate=Dstate-1.0;
 			break;
 		case 46:
-			if(Dstate<4)
-				Dstate=Dstate+1;
+			if(Dstate<4.0)
+				Dstate=Dstate+1.0;
 			break;
 		case 60:
-			if(Dstate*10%10!=0.0)
+			if((Dstate-floor(Dstate))!=0.0)
 				Dstate=Dstate-0.1;
 			break;
 		case 62:
