92cd3110554fa86605972df5493fb2496be1808d
DanTNC <fad11204@yahoo.com.tw>
2016-11-26 01:26:48 +0800

    Update MSOEctr.js

--- MSOEctr.js
+++ MSOEctr.js
@@ -1,7 +1,7 @@
 var Tstate=0; //0:A, 1:A  2:a  3:a'
 var Dstate=0.0; //n=-5~4. n=0 for N=1, n+1=>N*2, n-1=>N/2. n.1=N*(1+1/2), n.2=N*(1+1/2+1/4)... and so on
 
-var ChgDstt = () => {
+var key = () => {
 	switch(event.keyCode){
 		case 44:
 			if(Dstate>-5)
@@ -18,6 +18,8 @@ var ChgDstt = () => {
 		case 62:
 			Dstate=Dstate+0.1;
 			break;
+	// ----------Change Dstate-----------
+		
 		default:
 	}
 	console.log(Dstate);
@@ -25,11 +27,6 @@ var ChgDstt = () => {
 };
 
 window.onload = () => {
-	document.onkeypress=ChgDstt;
-	a();
-}
+	document.onkeypress=key;
 
-var a = () => {
-	console.log("123132");
 }
-
